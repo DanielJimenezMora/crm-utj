54ba99e6beedf3b7ed9cbf79493cfdd08fe681be
Daniel Jimenez Mora <danieljimenezmora@gmail.com>
2022-05-16 00:33:36 -0500

    Agregar usuarios a la base de datos

--- Documents/UTJ/Proyecto 9no/CRMGraphQL/db/resolvers.js
+++ Documents/UTJ/Proyecto 9no/CRMGraphQL/db/resolvers.js
@@ -1,7 +1,31 @@
+const Usuario = require("../models/usuario");
+
 // Resolvers
 const resolvers = {
   Query: {
     obtenerCursos: () => "Hola mundo",
+  },
+  Mutation: {
+    nuevoUsuario: async (_, { input }) => {
+      const { email, password } = input;
+
+      // Revisar si el usuario ya está registrado
+      const existeUsuario = await Usuario.findOne({ email });
+      console.log(existeUsuario);
+      if (existeUsuario) {
+        throw new Error("El usuario ya está registrado");
+      }
+      // Hashear el password
+
+      try {
+        // Guardar el usuario en la base de datos
+        const usuario = new Usuario(input);
+        usuario.save(); //Guardar el usuario en la base de datos
+        return usuario;
+      } catch (error) {
+        console.log(error);
+      }
+    },
   },
 };
 
