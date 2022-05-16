8cbdc056ea44f93d5d69fe44c1a9d58becc0993e
Daniel Jimenez Mora <danieljimenezmora@gmail.com>
2022-05-16 00:03:05 -0500

    Se aÃgrega models para DB al proyecto

---
+++ Documents/UTJ/Proyecto 9no/CRMGraphQL/models/Usuario.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const UsuariosSchema = mongoose.Schema({
+  nombre: {
+    type: "string",
+    required: true,
+    trim: true,
+  },
+  apellido: {
+    type: "string",
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: "string",
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  password: {
+    type: "string",
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  creado: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model("Usuario", UsuariosSchema);
